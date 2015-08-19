fd0ac3b894d3516a7cb8f8e02dac03b7a6299bf9
Nicolas Ochem <nicolas.ochem@gmail.com>
2015-08-18 23:36:23 -0700

    pass muse-io output to stdout
    in order to have feedback that the publisher is actually publishing
    something.

--- cloudbrain/connectors/MuseConnector.py
+++ cloudbrain/connectors/MuseConnector.py
@@ -25,10 +25,14 @@ def connect_muse(port=9090, mac=None):
     print "\nRunning command: %s" %" ".join(cmd)
 
     try:
-      muse_process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+      muse_process = subprocess.call(cmd, shell=False)
+      proc.communicate()
     except OSError:
       raise UnableToStartMuseIO("No able to start muse-io." + \
             "Muse-io is not installed. Go to http://choosemuse.com for more info.")
+    except KeyboardInterrupt:
+      print "Interrupted !"
+      exit(0)
 
     while True: # look for museIO running keywords
       line = muse_process.stdout.readline()
